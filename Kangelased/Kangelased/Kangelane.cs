e764dd5355a84ae22c0f2d321091fbdaf4063280
Karel Vendla <opilane@HP-CZC6297B8Q>
2019-05-15 14:33:23 +0300

    Muuda kangelased klassi, lisades superNimi ja v6ime muutujad. Konstruktorile  nende muutujate lisamine.

--- Kangelased/Kangelased/Kangelane.cs
+++ Kangelased/Kangelased/Kangelane.cs
@@ -12,15 +12,22 @@ namespace Kangelased
 
         private string asukoht;
 
-        public Kangelane(string nimi, string asukoht)
+        private string superNimi;
+
+        private string v6ime;
+
+        public Kangelane(string nimi, string asukoht, string superNimi, string v6ime)
         {
-            this.Nimi = nimi;
-            this.Asukoht = asukoht;
+            this.nimi = nimi;
+            this.asukoht = asukoht;
+            this.superNimi = superNimi;
+            this.v6ime = v6ime;
         }
 
         public string Nimi { get => nimi; set => nimi = value; }
         public string Asukoht { get => asukoht; set => asukoht = value; }
-
+        public string SuperNimi { get => superNimi; set => superNimi = value; }
+        public string V6ime { get => v6ime; set => v6ime = value; }
 
         public virtual int Päästa(int ohus)
         {
