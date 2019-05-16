e7a16087d3fcc3b15c17f5fad146cfca10eb13fa
Karel Vendla <opilane@HP-CZC6297B8Q>
2019-05-16 14:11:32 +0300

    Loo uued klassid HeaKangelane ja PahaKangelane

---
+++ Kangelased/Kangelased/HeaKangelane.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kangelased
+{
+    abstract class HeaKangelane : Kangelane
+    {
+        public HeaKangelane(string nimi, string superNimi, string v6ime) : base(nimi, superNimi, v6ime)
+        {
+
+        }
+    }
+}
---
+++ Kangelased/Kangelased/PahaKangelane.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kangelased
+{
+    abstract class PahaKangelane : Kangelane
+    {
+        public PahaKangelane(string nimi, string superNimi, string v6ime) : base(nimi, superNimi, v6ime)
+        {
+
+        }
+    }
+}
